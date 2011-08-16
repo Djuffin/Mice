52494f9e5f92fa4a175d668c1025ad3797284da0
Eugene Zemtsov <e.zemtsov@gmail.com>
2011-08-17 02:56:23 +0400

    Initial commit

---
+++ Cheese/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cheese
+{
+	public class Person
+	{
+		private bool _isAlive;
+		public bool IsAlive
+		{
+			get
+			{
+				return _isAlive;
+			}
+		}
+
+		private string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value;
+			}
+		}
+
+		public Person()
+		{
+			_isAlive = true;
+		}
+
+		public void Kill()
+		{
+			_isAlive = false;
+		}
+	}
+}
---
+++ Mice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace Mice
+{
+	class Program
+	{
+		const string victimName = "Cheese.dll";
+
+		static void Main(string[] args)
+		{
+			
+			var assembly = AssemblyDefinition.ReadAssembly(victimName);
+
+			foreach (var type in assembly.Modules.SelectMany(m => m.Types).ToArray())
+			{
+				Console.WriteLine(type.FullName);
+				if (type.IsPublic)
+				{
+					CreateStaticPrototype(type);
+				}
+				foreach (var method in type.Methods)
+				{
+					Console.WriteLine(method.FullName);
+				}
+			}
+
+			assembly.Write(victimName);
+		}
+
+		private static TypeDefinition CreateStaticPrototype(TypeDefinition type)
+		{
+			TypeDefinition result = new TypeDefinition(type.Namespace, type.Name + "Prototype", TypeAttributes.Public | TypeAttributes.Sealed);
+			type.Module.Types.Add(result);
+			
+			return result;
+		}
+	}
+}
